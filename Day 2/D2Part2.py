93dbcb19343ca83dfd4ec1433bd99738d3f6631e
Laura Sach <5183697+lawsie@users.noreply.github.com>
2024-12-12 11:25:52 +0000

    Finished day 2 (though I don't really like it)

---
+++ Day 2/D2Part2.py
@@ -0,0 +1,46 @@
+def test(levels):
+
+    # Check if increasing or decreasing
+    decreasing = True
+    if levels[0] < levels[1]:
+        decreasing = False
+
+    # The levels are either all increasing or all decreasing.
+    for i in range(len(levels) - 1):
+        if decreasing:
+            if levels[i] < levels[i + 1]:
+                return False
+        else:
+            if levels[i] > levels[i + 1]:
+                return False
+        # Any two adjacent levels differ by at least one and at most three.
+        diff = max(levels[i], levels[i + 1]) - min(levels[i], levels[i + 1])
+        if diff < 1 or diff > 3:
+            return False
+
+    return True
+
+
+with open("input.txt") as f:
+    report = f.readlines()
+
+report = [r.strip("\n").split(" ") for r in report]
+
+safe = 0
+
+for line in report:
+    line = [int(x) for x in line]
+    if test(line):
+        safe += 1
+    else:
+        # If it was unsafe, check whether removing any level makes it safe
+        secondchance = False
+        for i in range(len(line)):
+            newlist = list(line)
+            del newlist[i]
+            if test(newlist):
+                secondchance = True
+        if secondchance:
+            safe += 1
+
+print(safe)
